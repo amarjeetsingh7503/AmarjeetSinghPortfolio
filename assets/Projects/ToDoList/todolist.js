e851cb128fc43c201497c4a726f17879a3fc30cd
Amarjeet Singh <140692557+amarjeetsingh7503@users.noreply.github.com>
2023-08-31 20:55:16 +0530

    Add files via upload

---
+++ assets/Projects/ToDoList/todolist.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const input = document.querySelector("input");
+    const addButton = document.querySelector("#push");
+    const tasksList = document.querySelector("#tasks");
+  
+    addButton.addEventListener("click", addTask);
+    input.addEventListener("keyup", function (event) {
+      if (event.key === "Enter") {
+        addTask();
+      }
+    });
+  
+    function addTask() {
+      const taskText = input.value.trim();
+      if (taskText === "") return;
+  
+      const taskItem = document.createElement("div");
+      taskItem.className = "taskItem";
+      taskItem.innerHTML = `
+        <input id="checkbox" type="checkbox">
+        <p class="strikethrough" id="taskText">${taskText}</p>
+        <button class="removeButton">
+        <img id="delete" src="./img/delete.png" width="25px" alt="delete">
+        </button> 
+        `;
+      tasksList.appendChild(taskItem);
+  
+      input.value = "";
+  
+      const removeButtons = document.querySelectorAll(".removeButton");
+      removeButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+          button.parentElement.remove();
+        });
+      });
+    }
+  });
